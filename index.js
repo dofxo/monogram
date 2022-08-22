670f4134604f783b9ed1e1dd14766aa7da1a8bb9
dofxo <kargat504@gmail.com>
2022-08-22 20:47:14 +0430

    is now responsive / hamburger menu added

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ function newMenu() {
 }
 dropDownEl.addEventListener('click', newMenu)
 // script for dropDown menu END
+
+// hamburger menu event listener
+const rowClass = document.getElementsByClassName('row')
+const mainMenu = document.getElementById('main-menu')
+const hamburgerMenuIcon = document.getElementById('hamburger-menu-icon')
+const hamburgerMenu = document.getElementById('hamburger-menu')
+hamburgerMenuIcon.addEventListener('click', () => {
+    if (!hamburgerMenu.classList.contains('hamburger-menu-apear')) {
+        hamburgerMenu.classList.add('hamburger-menu-apear')
+        rowClass[0].classList.add('hamburger-icon-change-top')
+        rowClass[1].style.display = 'none'
+        rowClass[2].classList.add('hamburger-icon-change-bottom')
+    }
+    else {
+        hamburgerMenu.classList.remove('hamburger-menu-apear')
+        rowClass[0].classList.remove('hamburger-icon-change-top')
+        rowClass[1].style.display = ''
+        rowClass[2].classList.remove('hamburger-icon-change-bottom')
+
+    }
+
+})
+// hamburger menu event listener END
